9257d9f13908c66f242b9d76a8cab043fc1e5bc5
pedrosabel <pedrosabel08@gmail.com>
2023-05-17 21:35:44 -0300

    .

--- Projeto/Projeto_pizzaria/projeto_pizzaria/src/visao/TelaFornecedor.java
+++ Projeto/Projeto_pizzaria/projeto_pizzaria/src/visao/TelaFornecedor.java
@@ -68,30 +68,30 @@ public class TelaFornecedor extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(141, 147, 1321, 471);
 		contentPane.add(scrollPane);
-		
+
 		tabelaFornecedor = new JTable();
 		tabelaFornecedor.setFont(new Font("Yu Gothic UI", Font.PLAIN, 14));
 		tabelaFornecedor.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"ID", "Nome", "CNPJ", "Contato"
-			}
-		));
+				new Object[][] {
+				},
+				new String[] {
+						"ID", "Nome", "CNPJ", "Contato"
+				}
+				));
 		scrollPane.setViewportView(tabelaFornecedor);
-		
+
 		try {
 			FornecedorBD fornecedorBD = new FornecedorBD();
-			
+
 			DefaultTableModel model = (DefaultTableModel) tabelaFornecedor.getModel();
 			model.setNumRows(0);
-			
+
 			ArrayList<Fornecedor> lista = fornecedorBD.pesquisarFornecedor();
-			
+
 			for(int num = 0 ; num < lista.size() ; num ++) {
 				model.addRow(new Object [] {
 						lista.get(num).getId(),
@@ -101,35 +101,35 @@ public class TelaFornecedor extends JFrame {
 
 				});
 			}
-			
+
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null,"Erro no Listar Valores" + e);
 		}
-	
+
 		JLabel lblNewLabel = new JLabel("ID:");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		lblNewLabel.setBounds(119, 654, 86, 22);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Nome:");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		lblNewLabel_1.setBounds(380, 650, 230, 30);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("CNPJ:");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		lblNewLabel_2.setBounds(750, 650, 230, 30);
 		contentPane.add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Contato:");
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_3.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		lblNewLabel_3.setBounds(1157, 654, 230, 30);
 		contentPane.add(lblNewLabel_3);
-		
+
 		txtID = new JTextField();
 		txtID.setBackground(Color.WHITE);
 		txtID.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
@@ -137,28 +137,28 @@ public class TelaFornecedor extends JFrame {
 		txtID.setBounds(141, 691, 86, 30);
 		contentPane.add(txtID);
 		txtID.setColumns(10);
-		
+
 		txtNome = new JTextField();
 		txtNome.setBackground(Color.WHITE);
 		txtNome.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		txtNome.setBounds(464, 691, 230, 30);
 		contentPane.add(txtNome);
 		txtNome.setColumns(10);
-		
+
 		txtCNPJ = new JFormattedTextField();
 		txtCNPJ.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		txtCNPJ.setBounds(839, 691, 230, 30);
 		contentPane.add(txtCNPJ);
-		
+
 		MaskFormatter maskCNPJ;
 		try {
 			maskCNPJ = new MaskFormatter("##.###.###/####-##");
 			maskCNPJ.install(txtCNPJ);
 		} catch (ParseException e1) {
-			
+
 			e1.printStackTrace();
 		}
-		
+
 		JButton btnNewButton = new JButton("Cadastrar");
 		btnNewButton.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		btnNewButton.addActionListener(new ActionListener() {
@@ -170,8 +170,8 @@ public class TelaFornecedor extends JFrame {
 		});
 		btnNewButton.setBounds(141, 767, 176, 30);
 		contentPane.add(btnNewButton);
-		
-		
+
+
 		JButton btnNewButton_2 = new JButton("Selecionar");
 		btnNewButton_2.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -181,41 +181,41 @@ public class TelaFornecedor extends JFrame {
 		});
 		btnNewButton_2.setBounds(356, 767, 176, 30);
 		contentPane.add(btnNewButton_2);
-		
+
 		JButton btnNewButton_3 = new JButton("Alterar");
 		btnNewButton_3.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(tabelaFornecedor.getSelectedRowCount() > 0) {
-				AlterarFornecedor();
-				LimparCampos();
-				listarValores();
+					AlterarFornecedor();
+					LimparCampos();
+					listarValores();
 				}
 				else {
-				JOptionPane.showMessageDialog(null,"Selecione uma linha da tabela para alterar!");
+					JOptionPane.showMessageDialog(null,"Selecione uma linha da tabela para alterar!");
 				}
 			}
 		});
 		btnNewButton_3.setBounds(587, 767, 176, 30);
 		contentPane.add(btnNewButton_3);
-		
+
 		JButton btnNewButton_4 = new JButton("Excluir");
 		btnNewButton_4.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(tabelaFornecedor.getSelectedRowCount() > 0) {
-				excluirFornecedor();
-				listarValores();
-				LimparCampos();	
+					excluirFornecedor();
+					listarValores();
+					LimparCampos();	
 				}
 				else {
-				JOptionPane.showMessageDialog(null,"Selecione uma linha da tabela para excluir!");
+					JOptionPane.showMessageDialog(null,"Selecione uma linha da tabela para excluir!");
 				}
 			}
 		});
 		btnNewButton_4.setBounds(818, 767, 176, 30);
 		contentPane.add(btnNewButton_4);
-		
+
 		JButton btnNewButton_5 = new JButton("Limpar");
 		btnNewButton_5.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		btnNewButton_5.addActionListener(new ActionListener() {
@@ -225,7 +225,7 @@ public class TelaFornecedor extends JFrame {
 		});
 		btnNewButton_5.setBounds(1051, 767, 176, 30);
 		contentPane.add(btnNewButton_5);
-		
+
 		JButton btnNewButton_6 = new JButton("Voltar");
 		btnNewButton_6.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		btnNewButton_6.addActionListener(new ActionListener() {
@@ -238,55 +238,55 @@ public class TelaFornecedor extends JFrame {
 		});
 		btnNewButton_6.setBounds(1286, 767, 176, 30);
 		contentPane.add(btnNewButton_6);
-		
+
 		JLabel lblNewLabel_4 = new JLabel("Tela de Fornecedores");
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_4.setFont(new Font("Yu Gothic UI", Font.BOLD, 20));
 		lblNewLabel_4.setBounds(153, 68, 1321, 42);
 		contentPane.add(lblNewLabel_4);
-		
+
 		txtTelefone = new JFormattedTextField();
 		txtTelefone.setFont(new Font("Yu Gothic UI", Font.PLAIN, 17));
 		txtTelefone.setBounds(1232, 695, 230, 33);
 		contentPane.add(txtTelefone);
-		
+
 		MaskFormatter maskTelefone;
 		try {
 			maskTelefone = new MaskFormatter("(##)#####-####");
 			maskTelefone.install(txtTelefone);
 		} catch (ParseException e1) {
-			
+
 			e1.printStackTrace();
 		}
 	}
-	
+
 	private void SelecionarCampos() {
-		
+
 		if(tabelaFornecedor.getSelectedRowCount() > 0) {
-		int setar = tabelaFornecedor.getSelectedRow();
-		
-		txtID.setText(tabelaFornecedor.getModel().getValueAt(setar, 0).toString());
-		txtNome.setText(tabelaFornecedor.getModel().getValueAt(setar, 1).toString());
-		txtCNPJ.setText(tabelaFornecedor.getModel().getValueAt(setar, 2).toString());
-		txtTelefone.setText(tabelaFornecedor.getModel().getValueAt(setar, 3).toString());
-	}
+			int setar = tabelaFornecedor.getSelectedRow();
+
+			txtID.setText(tabelaFornecedor.getModel().getValueAt(setar, 0).toString());
+			txtNome.setText(tabelaFornecedor.getModel().getValueAt(setar, 1).toString());
+			txtCNPJ.setText(tabelaFornecedor.getModel().getValueAt(setar, 2).toString());
+			txtTelefone.setText(tabelaFornecedor.getModel().getValueAt(setar, 3).toString());
+		}
 		else {
 			JOptionPane.showMessageDialog(null,"Clique em uma linha da tabela para selecionar!");
 		}
 	}
-	
+
 	private void CadastrarFornecedor() {
 		String nome, cnpj, telefone;
-		
+
 		nome = txtNome.getText();
 		cnpj = txtCNPJ.getText();
 		telefone = txtTelefone.getText();
-		
+
 		Fornecedor fornecedor = new Fornecedor();
 		fornecedor.setNome(nome);
 		fornecedor.setCnpj(cnpj);
 		fornecedor.setTelefone(telefone);
-		
+
 		FornecedorBD fornecedorBD = new FornecedorBD();
 		fornecedorBD.cadastrarFornecedor(fornecedor);
 	}
@@ -300,43 +300,43 @@ public class TelaFornecedor extends JFrame {
 	private void AlterarFornecedor() {
 		int id;
 		String nome, cnpj, telefone;
-		
+
 		id = Integer.parseInt(txtID.getText());
 		nome = txtNome.getText();
 		cnpj = txtCNPJ.getText();
 		telefone = txtTelefone.getText();
-		
+
 		Fornecedor fornecedor = new Fornecedor();
 		fornecedor.setId(id);
 		fornecedor.setNome(nome);
 		fornecedor.setCnpj(cnpj);
 		fornecedor.setTelefone(telefone);
 
-		
+
 		FornecedorBD fornecedorBD = new FornecedorBD();
 		fornecedorBD.alterarFornecedor(fornecedor);
 	}
 	private void excluirFornecedor() {
 		int id;
-		
+
 		id = Integer.parseInt(txtID.getText());
-		
+
 		Fornecedor fornecedor = new Fornecedor();
 		fornecedor.setId(id);
-		
+
 		FornecedorBD fornecedorBD = new FornecedorBD();
 		fornecedorBD.excluirFornecedor(fornecedor);
 	}
-	
+
 	private void listarValores() {
 		try {
 			FornecedorBD fornecedorBD = new FornecedorBD();
-			
+
 			DefaultTableModel model = (DefaultTableModel) tabelaFornecedor.getModel();
 			model.setNumRows(0);
-			
+
 			ArrayList<Fornecedor> lista = fornecedorBD.pesquisarFornecedor();
-			
+
 			for(int num = 0 ; num < lista.size() ; num ++) {
 				model.addRow(new Object [] {
 						lista.get(num).getId(),
@@ -346,7 +346,7 @@ public class TelaFornecedor extends JFrame {
 
 				});
 			}
-			
+
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null,"Erro no Listar Valores" + e);
 		}
